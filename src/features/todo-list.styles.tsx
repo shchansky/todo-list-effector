0ce68917e7ee16d537572a84867bd737afbd55e3
mgavrilenko <gavrilenko@megapolis-it.ru>
2022-01-30 20:24:29 +0300

    интегрировал effector

--- src/features/todo-list.styles.tsx
+++ src/features/todo-list.styles.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 350px;
+  box-sizing: border-box;
 `;
 
 export const Form = styled.form`
@@ -21,7 +22,6 @@ export const Tasks = styled.ul`
   padding: 0;
   display: flex;
   flex-direction: column;
-  /* justify-content: start; */
   gap: 4px;
 `;
 
--- src/features/todo-list.tsx
+++ src/features/todo-list.tsx
@@ -1,22 +1,57 @@
 import React from "react";
+import { createStore, createEvent } from "effector";
+import { useStore } from "effector-react";
 import * as Markup from "./todo-list.styles";
 
 const data = new Array(1);
 
-export const TodoList = () => {
-  const [value, setValue] = React.useState("");
-  const handleChange = React.useCallback((e) => setValue(e.target.value), []);
+// export const TodoList = () => {
+//   const [value, setValue] = React.useState("");
+//   const handleChange = React.useCallback((e) => setValue(e.target.value), []);
 
+//   const handleClick = (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     data.push(value);
+//     setValue((prev) => (prev = ""));
+//   };
+
+//   return (
+//     <Markup.Container>
+//       <Markup.Form onSubmit={handleClick}>
+//         <Markup.Textarea value={value} onChange={handleChange} />
+//         <Markup.Button type="submit">Set!</Markup.Button>
+//       </Markup.Form>
+//       <hr />
+
+//       <Markup.Tasks>
+//         {data.map((el, index) => (
+//           <Markup.Task key={index}>{el}</Markup.Task>
+//         ))}
+//       </Markup.Tasks>
+//     </Markup.Container>
+//   );
+// };
+
+
+
+const valueChanged = createEvent<string>()
+const valueButtonClicked = createEvent();
+const $value = createStore("").on(valueChanged, (_oldValue, newValue) => newValue)
+
+export const TodoList = () => {
+  const value = useStore($value);
   const handleClick = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    data.push(value);
-    setValue((prev) => (prev = ""));
+    valueButtonClicked();
+    data.push(`${value}`);
+    console.log(value)
+    valueChanged("")
   };
 
   return (
     <Markup.Container>
       <Markup.Form onSubmit={handleClick}>
-        <Markup.Textarea value={value} onChange={handleChange} />
+        <Markup.Textarea value={value} onChange={(e) => valueChanged(e.target.value)} />
         <Markup.Button type="submit">Set!</Markup.Button>
       </Markup.Form>
       <hr />
