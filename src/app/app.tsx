70f4d519b77077df3416d9c8ad4ecfc71570f5c6
mgavrilenko <gavrilenko@megapolis-it.ru>
2022-02-11 20:21:27 +0300

    улучшение архитектуры

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-import logo from "./logo.svg";
 
-import "./App.css";
-import { TodoList } from "./features";
+import { TodoList } from "../features";
 
 function App() {
   return <TodoList />;
